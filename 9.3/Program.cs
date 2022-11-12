6897d34d5e8c777978e33929a5b60506f706a788
EdieLay <98664652+EdieLay@users.noreply.github.com>
2022-11-13 00:56:19 +0300

    9.3

--- 9.3/Program.cs
+++ 9.3/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public enum ClothingSize
+{
+    XXS = 32,
+    XS = 34,
+    S = 36,
+    M = 38,
+    L = 40
+}
+
+public static class ClothingSizeExtension
+{
+    public static string GetTypeOfSize(this ClothingSize value)
+    {
+        if ((int)value != 32) return "Взрослый размер";
+        else return "Детский размер";
+    }
+}
+
+interface IMenClothes
+{
+    void dressMan();
+}
+
+interface IWomenClothes
+{
+    void dressWoman();
+}
+
+abstract class Clothes
+{
+    public ClothingSize ClS { get; set; }
+    public int Cost { get; set; } = 0;
+    public string Colour { get; set; } = "";
+}
