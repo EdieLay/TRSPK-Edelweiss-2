84b9f58a0e1af20dc0dca1213011a7e165c44651
EdieLay <98664652+EdieLay@users.noreply.github.com>
2022-11-14 20:04:54 +0300

    Fixed Classes A, B

--- 9.1/Program.cs
+++ 9.1/Program.cs
@@ -1,15 +1,44 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Collections;
+
+A ClassA = new A(5, 5);
+A ClassA2 = new A(2, 2);
+A ClassA3 = new A(7, 8);
+CustomArray<A> cArray = new CustomArray<A>(2);
+cArray[0] = ClassA;
+cArray[1] = ClassA2;
+
+// Вывод op2 у всех элементов
+foreach (A item in cArray)
+{
+    Console.WriteLine(item.op2);
+}
+
+// Смена значений поля op2 у всех элементов
+foreach (A item in cArray)
+{
+    item.op2 = ClassA3.op2;
+}
+
+// Вывод op2 у всех элементов
+foreach (A item in cArray)
+{
+    Console.WriteLine(item.op2);
+}
+
+
+
 public interface ICustomComparable<T>
 {
     int CustomCompare(T val);
 }
 
-class CustomArray<T> where T: ICustomComparable<T>
+public class CustomArray<T> : IEnumerable where T: ICustomComparable<T>
 {
     T[] valueArr;
-    CustomArray(int count)
+    public CustomArray(int count)
     {
-        valueArr= new T[count];
+        valueArr = new T[count];
     }
     public void Sort(CustomArray<T> arr)
     {
@@ -27,6 +56,11 @@ class CustomArray<T> where T: ICustomComparable<T>
         }
     }
 
+    public IEnumerator GetEnumerator()
+    {
+        return valueArr.GetEnumerator();
+    }
+
     public T this[int index] 
     {
         get => valueArr[index];
@@ -34,12 +68,22 @@ class CustomArray<T> where T: ICustomComparable<T>
     }
 }
 
-class A: ICustomComparable<A>
+public class A: ICustomComparable<A>
 {
-    int op1 { get; set; }
-    int op2 { get; set; }
+    int _op1;
+    int _op2;
+    public int op1
+    {
+        get => _op1;
+        set => _op1 = value;
+    }
+    public int op2
+    {
+        get => _op2;
+        set => _op2 = value;
+    }
 
-    A(int op1, int op2)
+    public A(int op1, int op2)
     {
         this.op1 = op1;
         this.op2 = op2;
@@ -55,12 +99,22 @@ class A: ICustomComparable<A>
     }
 }
 
-class B : ICustomComparable<B>
+public class B : ICustomComparable<B>
 {
-    string op1 { get; set; }
-    double op2 { get; set; }
+    string _op1;
+    double _op2;
+    public string op1
+    {
+        get => _op1;
+        set => _op1 = value;
+    }
+    public double op2
+    {
+        get => _op2;
+        set => _op2 = value;
+    }
 
-    B(int op1, int op2)
+    public B(string op1, double op2)
     {
         this.op1 = op1;
         this.op2 = op2;
@@ -68,10 +122,12 @@ class B : ICustomComparable<B>
 
     public int CustomCompare(B val)
     {
-        if (this.op1 + this.op2 > val.op1 + val.op2)
+        if (this.op1.Length + this.op2 > val.op1.Length + val.op2)
             return 1;
-        if (this.op1 + this.op2 < val.op1 + val.op2)
+        if (this.op1.Length + this.op2 < val.op1.Length + val.op2)
             return -1;
         return 0;
     }
 }
+
+
